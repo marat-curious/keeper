2c2062c6cf444f27f26e0ab7f0efcd71a21ca69c
Marat Amirov <m.amirov@vk.team>
2023-10-07 18:26:24 +0300

    format code

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,21 @@
 open Printf
 
 let () =
-	let usage_msg = "Usage: keeper <action> -l <login> [-p <password>]\naction: add | edit | remove\narguments:" in
-	let action = ref "" in
-	let login = ref "" in
-	let password = ref "" in
-	let anon param = if !action == "" then action := param in
-	let args =
-		[
-			("-l", Arg.Set_string login, "login");
-			("-p", Arg.Set_string password, "password");
-		]
-	in
-	Arg.parse (Arg.align args) anon usage_msg;
-	printf "actions: %s\n" !action;
-  printf "login: %s, password: %s\n" !login !password;
+  let usage_msg =
+    "Usage: keeper <action> -l <login> [-p <password>]\n\
+     action: add | edit | remove\n\
+     arguments:"
+  in
+  let action = ref "" in
+  let login = ref "" in
+  let password = ref "" in
+  let anon param = if !action == "" then action := param in
+  let args =
+    [
+      ("-l", Arg.Set_string login, "login");
+      ("-p", Arg.Set_string password, "password");
+    ]
+  in
+  Arg.parse (Arg.align args) anon usage_msg;
+  printf "actions: %s\n" !action;
+  printf "login: %s, password: %s\n" !login !password
