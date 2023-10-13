08b31a408a397633ccf91f242d4a5da4a247eedb
Marat Amirov <m.amirov@vk.team>
2023-10-13 14:37:23 +0300

    find record in file and display it

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Printf
 
-type action = None | Add | Edit | Remove
+type action = None | Add | Get | Edit | Remove
 (* type record = { login : string; password : string } *)
 
 let default_filename = "passwords"
@@ -19,6 +19,7 @@ let () =
     user_action :=
       match arg with
       | "add" -> Add
+      | "get" -> Get
       | "edit" -> Edit
       | "remove" -> Remove
       | _ -> None
@@ -31,11 +32,39 @@ let () =
   in
   Arg.parse (Arg.align args) anon_args usage_msg;
 
+  let find record in_channel =
+    let read_line () =
+      try Some (input_line in_channel) with End_of_file -> None
+    in
+    let rec check_line = function
+      (* TODO: add check to invalid argument in String.sub function  *)
+      | Some line ->
+          if String.length line < String.length record then
+            check_line (read_line ())
+          else if String.sub line 0 (String.length record) = record then
+            Some line
+          else check_line (read_line ())
+      | None ->
+          close_in in_channel;
+          None
+    in
+    check_line (read_line ())
+  in
+
   let do_action = function
     | Add ->
-        let oc = Out_channel.open_gen [Open_append] 0o600 default_filename in
+        let oc = Out_channel.open_gen [ Open_append ] 0o600 default_filename in
         fprintf oc "%s\n" (!login ^ ":" ^ !password);
         close_out oc
+    | Get -> (
+        let ic = In_channel.open_gen [ Open_rdonly ] 0o600 default_filename in
+        match find !login ic with
+        | Some l -> print_endline l
+        | None ->
+            ();
+            flush stdout)
+    | Edit -> ()
+    | Remove -> ()
     | _ -> ()
   in
   do_action !user_action
